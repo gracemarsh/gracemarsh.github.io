22904d746e755ec2ccca955ea0e8f6e9d2e71963
Grace <gracemarsh27@gmail.com>
2021-09-16 19:23:11 +1000

    added typewriting advert function

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 // Need Multiple JavaScript interactions.
+
+// TYPEWRITING ADVERT
+
+const texts = [
+  "so slow?",
+  "not getting new leads?",
+  "outdated?",
+  "always breaking?",
+  "mobile incompatible?",
+  "confusing visitors?",
+  "lacking SEO?",
+  "lacking accessibility?",
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 150);
+})();
