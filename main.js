f62b70b9bd40633c5c6fe732263061f89dea5053
Grace <gracemarsh27@gmail.com>
2021-10-01 12:43:58 +1000

    updated

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 // Need Multiple JavaScript interactions.
 
-// MINIMISE HOME PAGE HEADER
-
-// function headerMinimise() {
-//   if (window.pageYOffset > 1000) {
-//     document.querySelector("header").classList.add("scrolled");
-//   }
-// }
-
-// window.addEventListener("scroll", headerMinimise);
-
 // SHOW ACTIVE NAV TAB
 
 let navLinks = document.querySelectorAll(".nav-link");
@@ -59,7 +49,6 @@ let letter = "";
   }
   if (count === texts.length) {
     count = 0;
-    console.log("first step");
   }
   currentText = texts[count];
   letter = currentText.slice(0, ++index);
@@ -70,32 +59,17 @@ let letter = "";
     count++;
     index = 0;
     setTimeout(type, 1500);
-    console.log("it's kind of working");
   } else {
     setTimeout(type, 150);
   }
 })();
 
-//BACK TO TOP SMOOTH SCROLLING
+// MINIMISE HOME PAGE HEADER
 
-//Get the button:
-const button = document.getElementById("to-top-btn");
+// function headerMinimise() {
+//   if (window.pageYOffset > 1000) {
+//     document.querySelector("header").classList.add("scrolled");
+//   }
+// }
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// window.addEventListener("scroll", headerMinimise);
